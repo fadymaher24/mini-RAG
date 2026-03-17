0b29bc7ac3737c4341a37b8d2ee1999682e18e23
fadymaher24 <fady.maher2024@gmail.com>
2026-03-17 16:52:46 +0200

    Enhance LLM integration: update LLMEnums with new roles, implement CoHereProvider for text generation and embedding, and modify LLMInterface for additional methods

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -1,14 +1,28 @@
 from enum import Enum
 
 
-class LLMEnum(str, Enum):
+class LLMEnum(Enum):
     OPENAI = "OPENAI"
     ANTHROPIC = "ANTHROPIC"
     AZURE_OPENAI = "AZURE_OPENAI"
     COHERE = "COHERE"
 
 
-class OpenAIEnums(str, Enum):
+class OpenAIEnums(Enum):
     SYSTEM = "system"
     USER = "user"
     ASSISTANT = "assistant"
+
+
+class CohereEnums(Enum):
+    SYSTEM = "SYSTEM"
+    USER = "USER"
+    ASSISTANT = "CHATBOT"
+
+    DOCUMENT = "search_document"
+    QUERY = "search_query"
+
+
+class DocumentTypeEnums(Enum):
+    DOCUMENT = "document"
+    QUERY = "query"
--- src/stores/llm/LLMInterface.py
+++ src/stores/llm/LLMInterface.py
@@ -11,6 +11,7 @@ class LLMInterface(ABC):
     def set_embedding_model(self, model_id: str, embedding_size: int):
         pass
 
+
     @abstractmethod
     def generate_text(
         self,
