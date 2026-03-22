458eb947037167e49007ead72f7f72b447ca6497
fadymaher24 <fady.maher2024@gmail.com>
2026-03-22 15:49:39 +0200

    Refactor VectorDBProviderFactory: remove inheritance from BaseController and update create method parameter type; add QdrantDBProvider implementation with connection and collection management methods.

--- src/stores/vectordb/VectorDBProviderFactory.py
+++ src/stores/vectordb/VectorDBProviderFactory.py
@@ -3,12 +3,12 @@ from .VectorDBEnums import VectorDBEnums
 from controllers.BaseController import BaseController
 
 
-class VectorDBProviderFactory(BaseController):
+class VectorDBProviderFactory:
     def __init__(self, config):
         self.config = config
         self.base_controller = BaseController()
 
-    def create(self, provider: dict):
+    def create(self, provider: str):
         if provider == VectorDBEnums.QDRANT.value:
             db_path = self.base_controller.get_database_path(
                 db_name=self.config.VECTOR_DB_PATH
@@ -18,4 +18,5 @@ class VectorDBProviderFactory(BaseController):
                 db_path=db_path,
                 distance_method=self.config.VECTOR_DB_DISTANCE_METHOD,
             )
+
         return None
